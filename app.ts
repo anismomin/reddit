b0dba3c884457620054e8592405ede78803f981c
anismomin <anismomin.dev@gmail.com>
2016-01-16 13:07:04 +0500

    break app in componnents

--- app.ts
+++ app.ts
@@ -1,98 +1,7 @@
 import {Component} from 'angular2/core'
 import {bootstrap} from 'angular2/platform/browser'
-
-class Article {
-
-	votes: number;
-	title: string;
-	link: string;
-
-	constructor(title: string, link: string, votes?: number) {
-		this.title = title;
-		this.link = link;
-		this.votes = votes || 0;
-
-	}
-
-	voteUp(): void {
-		this.votes += 1;
-
-	}
-
-	voteDown() {
-		this.votes -= 1;
-
-	}
-
-	domain(): string {
-		try {
-			const link: string = this.link.split('//')[1];
-			return link.split('/')[0];
-
-		} catch (err) {
-			return null;
-
-		}
-
-	}
-
-
-
-
-}
-
-@Component({
-	selector: 'reddit-article',
-	inputs: ['article'],
-	host: {
-		class: 'row'
-	},
-	template: `
-		 <div class="four wide column center aligned votes">
-			 <div class="ui statistic">
-				 <div class="value">
-				 {{ article.votes }}
-				 </div>
-			 	<div class="label">Points</div>
-			 </div>
-		 </div>
-		 <div class="twelve wide column">
-			 <a class="ui large header" href="{{ article.link }}">{{ article.title }}</a>
-			 <div class="meta">({{ article.domain() }})</div>
-			 <ul class="ui big horizontal list voters">
-			 	<li class="item">
-			 		<a href (click)="voteUp()">
-			 			<i class="arrow up icon"></i> upvote
-					</a>
-			 	</li>
-			 	<li class="item">
-			 		<a href (click)="voteDown()">
-			 			<i class="arrow down icon"></i> downvote
-			 		</a>
-			 	</li>
-			 </ul>
-		 </div>
-			 `
-})
-class ArticleComponent {
-	
-	article: Article;
-
-	constructor() {
-		this.article = new Article('Angular 2', 'http://angular.io');
-	}
-
-	voteUp(): boolean {
-		this.article.voteUp();
-		return false;
-	}
-
-	voteDown(): boolean {
-		this.article.voteDown();
-		return false;
-	}
-
-}
+import {Article} from './article'
+import {ArticleComponent} from './article-component'
 
 @Component({
 	selector: 'reddit',
@@ -124,9 +33,9 @@ class RedditApp {
 
 	constructor() {
 		this.articles = [
-			new Article('angular 1', 'http://angular.io', 2),
-			new Article('angular 2', 'http://angular2.io', 23),
-			new Article('angular 3', 'http://angular3.io', 5),
+			new Article('anismomin', 'http://github.com/anismomin', 500),
+			new Article('facebook', 'http://facebook.com/anismomin', 400),
+			new Article('angular', 'http://angular.io', 700),
 		];
 
 	}
